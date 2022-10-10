64ea1af20559239722b228b11539eeea66174f89
jeteachingresearch <jeteachingresearch@gmail.com>
2022-10-10 18:44:06 +0200

    Stack is updated: Javadoc.

--- src/Stack/Stack.java
+++ src/Stack/Stack.java
@@ -1,13 +1,55 @@
 package Stack;
 
+import java.util.NoSuchElementException;
+
+/**
+ * A linear collection that only supports element insertion and removal at the top of it. Concretely, it is a last-in-first-out (LIFO) stack of objects.
+ *
+ * <p> Methods are provided to insert an element in the top, to obtain or to obtain and remove the top of the stack, to know if the stack is empty and, to know the number of elements in the collection.</p>
+ *
+ * @author Juan Enrique and Juan Manuel
+ *
+ * @param <E> defines the type of the elements in the Stack
+ */
+
 public interface Stack<E> {
+
+    /**
+     * Pushes an item in the top of the stack. If there is no space, it throws an {@code IllegalStateException}.
+     *
+     * @throws IllegalStateException if the item cannot be pushed due to capacity restrictions.
+     * */
     void push(E element);
 
+    /**
+     * Returns and eliminates the item which is in the top of the stack.
+     *
+     * @return the element which is in the top of the stack.
+     *
+     * @throws NoSuchElementException if the stack is empty.
+     * */
     E pop();
 
+    /**
+     * Returns the element which is in the top of the stack.
+     *
+     * @return the element which is in the top of the stack.
+     *
+     * @throws NoSuchElementException if this stack is empty.
+     * */
     E top();
 
+    /**
+     * Tests if the stack is empty.
+     *
+     * @return {@code true} if the stack does not contain items. Otherwise, {@code false}.
+     * */
     boolean isEmpty();
 
+    /**
+     * Returns the number of items in the stack.
+     *
+     * @return the number of items in the stack.
+     * */
     int size();
 }
